575925c9d82fa635ca65ee07771117fb377014ea
Ayomide <ogunyemiayomide700@gmail.com>
2025-04-28 22:04:02 +0100

    calc function

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -1,13 +1,66 @@
 import { useState } from "react";
 import CalculatorBtn from "./CalculatorBtn";
+import { Parser } from "expr-eval";
 
 const App = () => {
-  const calculatorBtn = "C,+/-,%,÷,7,8,9,×,4,5,6,-,1,2,3,+,0,.,=";
+  const calculatorBtn = "C,del,%,÷,7,8,9,x,4,5,6,-,1,2,3,+,0,.,=";
   const split = calculatorBtn.split(",");
 
-  const [calcBtn, setCalcBtn] = useState(0);
+  const [displayCalc, setDisplayCalc] = useState("0");
+
   const handleBtnClick = (e: any) => {
-    setCalcBtn((prev) => {});
+    const value = e.target.value;
+    if (value === "C") {
+      setDisplayCalc("0");
+    } else if (value === "=") {
+      calculate();
+    } else if (value === "%") {
+      percentage();
+    } else if (value === "del") {
+      backSpace();
+    } else {
+      setDisplayCalc((prev) => {
+        if (prev === "0") {
+          return value;
+        } else {
+          return prev + value;
+        }
+      });
+    }
+  };
+
+  const calculate = () => {
+    try {
+      const expression = displayCalc.replace(/x/g, "*").replace(/÷/g, "/");
+
+      const parser = new Parser();
+      const expr = parser.parse(expression);
+      const result = expr.evaluate(expression);
+      setDisplayCalc(result.toString());
+    } catch (error) {
+      setDisplayCalc("error");
+    }
+  };
+
+  const backSpace = () => {
+    setDisplayCalc((prev) => {
+      if (prev.length <= 1) {
+        return "0";
+      } else {
+        return prev.slice(0, -1);
+      }
+    });
+  };
+
+  const percentage = () => {
+    setDisplayCalc((prev) => {
+      const parts = prev.split(/([\+\-x÷])/);
+      const last = parts.pop() ?? "";
+      const op = parts.pop() ?? "";
+      const percent = (parseFloat(last) / 100).toString();
+
+      return parts.join("") + op + percent;
+    });
   };
 
   return (
@@ -15,7 +68,7 @@ const App = () => {
       <div className="w-full h-screen flex justify-center items-center">
         <div className="w-[380px] h-[550px] rounded-[15px] bg-gray-300 pt-7 px-3 pb-5 flex flex-col  ">
           <div className="w-full h-[150px] bg-white rounded-[15px] p-4 ">
-            {calcBtn}
+            {displayCalc}
           </div>
           <div className="mt-7 flex-1 rounded-b-xl grid gap-4 grid-cols-4 grid-rows-5 ">
             {split.map((btn, i) => (
